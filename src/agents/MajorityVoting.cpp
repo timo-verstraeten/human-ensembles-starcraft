64f4e5359115fa821993eb1302b65b53437b7364
Roxana Radulescu <rradules@vub.ac.be>
2015-05-02 16:18:41 +0200

    Fixed Majority Voting with Q-values for tie cases

--- src/agents/MajorityVoting.cpp
+++ src/agents/MajorityVoting.cpp
@@ -1,8 +1,12 @@
 #include "MajorityVoting.h"
 #include "QValuesAgent.h"
 
+#include "../env/FunctionApproximator.h"
+
+#include <algorithm>
 #include <numeric>
 #include <sstream>
+#include <vector>
 
 MajorityVoting::MajorityVoting(const std::vector<QValuesAgent*> &agents, double epsilon)
 	: PreferenceBasedEnsembleAgent(std::vector<SMDPAgent*>(agents.begin(), agents.end()), epsilon)
@@ -17,14 +21,20 @@ double MajorityVoting::preferenceValue(const State &state, Action action) const
 {
 	std::stringstream dummy;
 	unsigned int count = 0;
-	
 
 	for (unsigned int i = 0; i < m_agents.size(); ++i) {
+
 		QValuesAgent* agent = static_cast<QValuesAgent*>(m_agents[i]);
+		std::vector<double> Qvalues(NUMBER_OF_ACTIONS);
 
-		Action maxAction = m_policy.selectQAction(agent->functionApproximator(), dummy);
+		for (unsigned int a = 0; a < NUMBER_OF_ACTIONS; ++a) {
+			Qvalues[a] = agent->functionApproximator().computeQ(static_cast<Action>(a));
+		}
 
-		if (action == maxAction) {
+		double maxQ = *std::max_element(Qvalues.begin(), Qvalues.end());
+		double q = agent->functionApproximator().computeQ(static_cast<Action>(action));
+
+		if (q == maxQ) {
 			++count;
 		}
 	}
